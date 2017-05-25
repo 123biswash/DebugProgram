b7e3e99fdab62206b9787f72020f8bf4be0e730e
Biswash Adhikari <biswash.adhikari@bison.howard.edu>
2017-05-25 15:53:47 -0400

    first commit

---
+++ debug.java
@@ -0,0 +1,13 @@
+package biswash_debug;
+public class debug {
+ public static void main(String[] args) {
+ double x;
+ double y;
+ 
+ x = 3.1415;
+ y = 3.64;
+ 
+ System.out.println("Pi is approximately " + x);
+ System.out.println("My GPA was " + y);
+ }
+}
